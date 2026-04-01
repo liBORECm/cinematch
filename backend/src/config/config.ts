0b39bea3c440a7bf90f5912b3744f825dcad502b
liBORECm <liborec.macka@seznam.cz>
2026-04-01 08:06:33 +0200

    Setting up env and prettier

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -4,10 +4,12 @@ dotenv.config();
 
 interface Config {
   port: number;
+  mlApiIP: string
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
+  mlApiIP: process.env.ML_API_IP || "http.//localhost:5000"
 };
 
 export default config;
