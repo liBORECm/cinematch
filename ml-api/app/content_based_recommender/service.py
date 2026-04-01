f804a289612b3af4cf3358faa2c6f1534df1cfb9
liBORECm <liborec.macka@seznam.cz>
2026-04-01 05:54:47 +0200

    Filtering already rated movies

--- ml-api/app/content_based_recommender/service.py
+++ ml-api/app/content_based_recommender/service.py
@@ -19,6 +19,7 @@ class ContenBasedRecommender:
         weights = []
 
         for tmdb_id, rating in ratings_dict.items():
+            tmdb_id = int(tmdb_id)
             if tmdb_id in self.tmdb_id_to_index:
                 idx = self.tmdb_id_to_index[tmdb_id]
                 vectors.append(self.tfidf_matrix[idx].toarray()[0])
@@ -33,12 +34,20 @@ class ContenBasedRecommender:
         return user_profile
 
     def recommend(self, ratings, k):
+        k_temp = k + len(ratings.items())
         user_vector = self.build_user_profile(ratings)
-
         sims = cosine_similarity(user_vector, self.tfidf_matrix).flatten()
-        top_idx = sims.argsort()[-k:][::-1]
+        top_idx = sims.argsort()[-k_temp:][::-1]
 
-        results = self.tmdb.iloc[top_idx][["title", "vote_average", "id"]]
+        rated_ids = set(int(tmdb_id) for tmdb_id in ratings.keys())
+        filtered_idx = []
+        for idx in top_idx:
+            movie_id = self.tmdb.iloc[idx]["id"]
+
+            if movie_id not in rated_ids:
+                filtered_idx.append(idx)
+
+        results = self.tmdb.iloc[filtered_idx][["title", "vote_average", "id"]]
 
         return results.to_dict(orient="records")
 
