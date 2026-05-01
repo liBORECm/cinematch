a248715b60b1bdeac101e9c67e83f628fb6f22af
liBORECm <liborec.macka@seznam.cz>
2026-05-01 22:54:14 +0200

    Removed route for anaylsis that wont be used anyway

--- ml-api/app/routes.py
+++ ml-api/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .content_based_recommender.service import content_based_recommender
+from .collaborative_recommender.service import collaborative_recommender
 
 bp = Blueprint("routes", __name__)
 
@@ -10,25 +11,11 @@ def recommend_content_based():
     ratings = data.get("ratings", [])
     k = data.get("k", 20)
 
-    print(f"Received request: \n    k: {k}\n    ratings: {ratings}")
+    print(f"Received request for content: \n    k: {k}\n    ratings: {ratings}")
 
     try:
         result = content_based_recommender.recommend(ratings, k)
-        return jsonify(result), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-    
-@bp.route("/api/v1/check_recommended_content_based", methods=["POST"])
-def check_recommended_content_based():
-    data = request.get_json()
-
-    ratings = data.get("ratings", [])
-    movie_id = data.get("movie_id", 1)
-
-    print(f"Received request: \n    movie_id: {movie_id}\n    ratings: {ratings}")
-
-    try:
-        result = content_based_recommender.check_recommended(ratings, movie_id)
+        print(f"Returning predicted: {result}")
         return jsonify(result), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 400
