dfb4a5974f1bbd05d6de6105872ff8a3d606b3aa
liBORECm <liborec.macka@seznam.cz>
2026-05-01 23:34:03 +0200

    idk

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,13 +21,3 @@ app.use("/api/v1/movie-db", movieDBRoutes)
 app.listen(config.port, () => {
   console.log(`Server running on http://localhost:${config.port}`)
 })
-
-
-/* contentBasedRecommender.recommend(new UserProfile([
-    { imdbId: 569094, rating: 1 },
-    { imdbId: 634649, rating: 0.5 },
-    { imdbId: 324857, rating: 1.5 },
-    { imdbId: 524434, rating: -2.5 },
-    { imdbId: 18224, rating: -3 },
-  ],
-)).then((res) => console.log(res)) */
